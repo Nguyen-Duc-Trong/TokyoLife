6d8b0de6a92b56ac16b52cbb3dd1620861558db3
Nguyen Anh Duong <anhduong21112001@gmail.com>
2023-10-08 12:24:23 +0700

    update

--- src/app/account/register/page.tsx
+++ src/app/account/register/page.tsx
@@ -3,10 +3,7 @@ import Register from "@/components/Register/Register"
 export default function Home() {
     return (
         <>
-            <div className='container'>
                 <Register/>
-
-            </div>
         </>
     )
 }
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -7,141 +7,110 @@ import { v4 as uuidv4 } from "uuid";
 
 function Register() {
   const [form] = Form.useForm();
-  // const [dataUser, setDataUser] = useState({
-  //   firstname: "",
-  //   lastname: "",
-  //   gender: "",
-  //   email: "",
-  //   date: "",
-  //   phone: "",
-  //   password: "",
-  // });
-  // const handleMultibleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setDataUser({ ...dataUser, [name]: value });
-  // };
-  // const handleSubmit = async () => {
-  //   try {
-  //     const data = {
-  //       username: dataUser.lastname + dataUser.firstname,
-  //       gender: dataUser.gender,
-  //       email: dataUser.email,
-  //       date: 1696164195,
-  //       phone: dataUser.phone,
-  //       password: dataUser.password,
-  //       token: uuidv4(),
-  //     };
-  //     console.log("dâta", data);
-  //     await fetch("https://64e35dd6bac46e480e78aabe.mockapi.io/api/v1/users", {
-  //       method: "POST",
-  //       headers: { 'content-type':'application/json'},
-  //       body: JSON.stringify(data),
-  //     }).then((res) => {
-  //       if (res.ok) {
-  //         console.log("dm that nghiep:)))))))");
-  //         console.log(res);
-          
-  //         return res.json()
-  //       }
-  //     }).then(res=>{
-  //       console.log(res);
-        
-  //     });
-  //   } catch (error) {
-  //     console.log("error: ", error);
-  //   }
-  // };
+  const onFinish = (value: any) => {
+    console.log(form.getFieldsValue());
+    console.log("value", value.firstname);
+  };
+  const PATTERN_PASSWORD =
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=])[A-Za-z\d@#$%^&+=]{6,12}$/;
+  const PATTERN_EMAIL =
+    /^[\s\S]*(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))(?= *$)/;
+
+  const PATTERN_PHONE_NUMBER = /^(0[3|5|7|8|9])([0-9]{8})\b$/;
+
   return (
-    <div className="bg-[#f7f7f7] flex justify-center w-full">
-      <div className="w-[620px] mt-[35px] mb-[50px] p-[30px] bg-[#fff] border">
-        <div className="title-register text-center text-[25px] font-bold">
-          <h1>Tạo tài khoản</h1>
-        </div>
-        <Form method="POST ">
-          <Form.Item>
-            <Input
-              name="firstname"
-              className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
-              // value={dataUser.firstname}
-              // onChange={handleMultibleChange}
-              placeholder="Họ"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              name="lastname"
-              // value={dataUser.lastname}
-              className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
-              placeholder="Tên"
-              // onChange={handleMultibleChange}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Radio.Group
-              name="gender"
-              // onChange={handleMultibleChange} 
-              // defaultValue={dataUser.gender}
+    <div className="container">
+      <div className="bg-[#f7f7f7] flex justify-center w-full">
+        <div className="w-[620px] mt-[35px] mb-[50px] p-[30px] bg-[#fff] border">
+          <div className="title-register text-center text-[25px] font-bold">
+            <h1>Tạo tài khoản</h1>
+          </div>
+          <Form
+            onFinish={onFinish}
+            form={form}
+            initialValues={{ remember: true }}
+          >
+            <Form.Item name="firstname">
+              <Input
+                className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
+                placeholder="Họ"
+              />
+            </Form.Item>
+            <Form.Item name="lastname">
+              <Input
+                className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
+                placeholder="Tên"
+              />
+            </Form.Item>
+            <Form.Item name="gender">
+              <Radio.Group>
+                <Radio value={1}>Nữ</Radio>
+                <Radio value={2}>Nam</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item name="date">
+              <Input
+                className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
+                placeholder="mm/dd/yyyy"
+              />
+            </Form.Item>
+            <Form.Item name="email">
+              <Input
+                className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
+                placeholder="Email"
+              />
+            </Form.Item>
+            <Form.Item
+              name="phoneNumber"
+              rules={[
+                { required: true },
+                {
+                  pattern: PATTERN_PHONE_NUMBER,
+                  message: "Định dạng số điện thoại không đúng",
+                },
+              ]}
             >
-              <Radio value={"female"}>Nữ</Radio>
-              <Radio value={"male"}>Nam</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item>
-            <Input
-              // value={dataUser.date}
-              name="date"
-              className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
-              placeholder="mm/dd/yyyy"
-              // onChange={handleMultibleChange}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              // value={dataUser.email}
-              name="email"
-              className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
-              placeholder="Email"
-              // onChange={handleMultibleChange}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              // value={dataUser.phone}
-              name="phone"
-              className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
-              placeholder="Số điện thoại"
-              // onChange={handleMultibleChange}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              // value={dataUser.password}
+              <Input
+                className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
+                placeholder="Số điện thoại"
+              />
+            </Form.Item>
+            <Form.Item
               name="password"
-              type="password"
-              className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
-              placeholder="Mật khẩu"
-              // onChange={handleMultibleChange}
-            />
-          </Form.Item>
-        </Form>
-        <div className="text-[#9e9e9e] text-[13px]">
-          This site is protected by reCAPTCHA and the Google
-          <a href="" className="mx-[5px] text-[#2962ff]">
-            Privacy Policy
-          </a>
-          and
-          <a href="" className="mx-[5px] text-[#2962ff]">
-            Terms of Service
-          </a>{" "}
-          apply.
+              rules={[
+                {
+                  pattern: PATTERN_PASSWORD,
+                  message:
+                    "Mật khẩu: 6-12 ký tự, bao gồm chữ hoa, chữ thường, số, ký tự đặc biệt",
+                },
+              ]}
+            >
+              <Input.Password
+                className="border-none h-[55px] text-[#5c5c5c] font-medium bg-[#ededed]"
+                placeholder="Mật khẩu"
+              />
+            </Form.Item>
+            <div className="text-[#9e9e9e] text-[13px]">
+              This site is protected by reCAPTCHA and the Google
+              <Link href="" className="mx-[5px] text-[#2962ff]">
+                Privacy Policy
+              </Link>
+              and
+              <Link href="" className="mx-[5px] text-[#2962ff]">
+                Terms of Service
+              </Link>{" "}
+              apply.
+            </div>
+            <Form.Item>
+              <Button
+                htmlType="submit"
+                className="mt-[25px] flex justify-center items-center mx-auto text-[14px] font-medium px-[40px] h-[45px] rounded-none text-[#fff] bg-[#111]"
+              >
+                Đăng kí
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
-        <Button
-          // onClick={handleSubmit}
-          className="mt-[25px] flex justify-center items-center mx-auto text-[14px] font-medium px-[40px] h-[45px] rounded-none text-[#fff] bg-[#111]"
-        >
-          Đăng kí
-        </Button>
       </div>
     </div>
   );
